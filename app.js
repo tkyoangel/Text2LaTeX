c5b915cfbb908753bcd957c1c328fb0838c165cb
Nokami <77856367+tkyoangel@users.noreply.github.com>
2024-06-25 20:34:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+function alpha() {
+    let textArea = document.getElementById("latex");
+    textArea.value += "\\" + "alpha"
+}
+
+function omega() {
+    let textArea = document.getElementById("latex");
+    textArea.value += "\\" + "omega"
+}
+
+function omegaMaj() {
+    let textArea = document.getElementById("latex");
+    textArea.value += "\\" + "Omega"
+}
+
+function phi() {
+    let textArea = document.getElementById("latex");
+    textArea.value += "\\" + "phi"
+}
+
+function pi(){
+    let textArea = document.getElementById("latex");
+    textArea.value += "\\" + "pi"
+}
+
+function piMaj(){
+    let textArea = document.getElementById("latex");
+    textArea.value += "\\" + "Pi"
+}
+
 function integrale() {
     // Obtenir le modal
     var modal = document.getElementById("myModal");
@@ -15,6 +45,15 @@ function somme() {
     document.getElementById("label2").innerHTML = "Borne supérieure de la somme"
     document.getElementById("label3").innerHTML = "Contenu de la somme"
     document.getElementById("modalType").innerHTML = "Ajout d'une somme"
+    modal.style.display = "block";
+}
+
+function limite() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("label1").innerHTML = ""
+    document.getElementById("label2").innerHTML = "Borne supérieure de l'intégrale"
+    document.getElementById("label3").innerHTML = "Fonction f(x)"
+    document.getElementById("modalType").innerHTML = "Ajout d'une limite"
     modal.style.display = "block";
 }
 
@@ -41,4 +80,35 @@ function submitModal() {
         let textArea = document.getElementById("latex")
         textArea.value += "\\" + "sum_{" + a + "}^{" + b + "} " + c;
     }
+}
+
+document.addEventListener('keyup', function(event){
+    if ((event.key === ' ' || event.code === "Space") && (document.getElementById("mode").innerHTML === 'MODE EQUATION')) {
+        espace();
+    }
+    if (event.key === "Control") {
+        changeMode();
+    }
+});
+
+function espace() {
+    let textArea = document.getElementById("latex");
+    textArea.value += "\\" + ";"
+}
+
+function changeMode() {
+    let mode = document.getElementById("mode");
+    mode.innerHTML = (mode.innerHTML === "MODE EQUATION" ? "MODE TEXTE" : "MODE EQUATION");
+    
+    
+    
+    
+    
+    // if (mode.innerHTML === "MODE EQUATION"){
+
+    // }
+
+    // if (mode === "MODE TEXTE"){
+        
+    // }
 }
